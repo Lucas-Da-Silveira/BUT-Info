49994dd7dff0ade63dca1c857d0d7c329cea5832
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-03-28 11:43:20 +0200

    Graphes TP1

--- 1-année/S2/TP/Graphes/TP1.py
+++ 1-année/S2/TP/Graphes/TP1.py
@@ -10,7 +10,7 @@ G.add_edge(1,2)
 G.add_edges_from([(1,3),(1,4),(2,3)])
 G.add_edge(1,5)
 
-nx.draw(G, with_labels=True, node_color='yellow')
+# nx.draw(G, with_labels=True, node_color='yellow')
 # plt.show()  
 
 print(G.nodes())
@@ -24,7 +24,7 @@ degrees = G.degree()
 print(sum(dict(degrees).values())/ G.number_of_nodes())
 
 
-A = nx.random_regular_graph(2,5)
+A = nx.random_regular_graph(2, 10)
 
 
 def regulier(G):
@@ -41,6 +41,44 @@ def regulier(A):
             return False
     return True
 
+def regulier(B):
+    degrees = B.degree()
+    for i in degrees:
+        if i[1] != 2:
+            return False
+    return True
+
+
 print(regulier(G))
 print(regulier(A))
 
+print(A)
+
+print(A.edges())
+
+print(A.number_of_edges())
+
+print(nx.diameter(A))
+
+
+B = nx.Graph()
+B.add_nodes_from([1,2,3,4,5,6])
+B.add_edges_from([(1,2),(1,5),(1,4),(2,3),(2,5),(2,4),(3,6),(3,4),(3,5),(4,5),(4,6)])
+# nx.draw(B, with_labels=True, node_color='yellow')
+# plt.show()
+
+def stats():
+    print("Nombre de noeuds : ", B.number_of_nodes())
+    print("Nombre d'arêtes : ", B.number_of_edges())
+    print("Degré moyen : ", sum(dict(B.degree()).values())/ B.number_of_nodes())
+    print("Diamètre : ", nx.diameter(B))
+    print("Graph régulier : ",regulier(B))
+
+stats()
+
+print("=====G======")
+print(nx.adjacency_matrix(G).toarray())
+print("=====A======")
+print(nx.adjacency_matrix(A).toarray())
+print("=====B======")
+print(nx.adjacency_matrix(B).toarray())
