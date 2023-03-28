328cec528a3b0550665691b4b68acddc3fd45f84
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-03-28 11:18:36 +0200

    Graphes TP1

--- 1-année/S2/TP/Graphes/TP1.py
+++ 1-année/S2/TP/Graphes/TP1.py
@@ -11,7 +11,7 @@ G.add_edges_from([(1,3),(1,4),(2,3)])
 G.add_edge(1,5)
 
 nx.draw(G, with_labels=True, node_color='yellow')
-plt.show()  
+# plt.show()  
 
 print(G.nodes())
 
@@ -22,3 +22,25 @@ print(G.degree(1))
 degrees = G.degree()
 
 print(sum(dict(degrees).values())/ G.number_of_nodes())
+
+
+A = nx.random_regular_graph(2,5)
+
+
+def regulier(G):
+    degrees = G.degree()
+    for i in degrees:
+        if i[1] != 2:
+            return False
+    return True
+
+def regulier(A):
+    degrees = A.degree()
+    for i in degrees:
+        if i[1] != 2:
+            return False
+    return True
+
+print(regulier(G))
+print(regulier(A))
+
