d3e399c9bc3bf79f940f364f91dbc368ce7dc1d3
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-03-28 10:57:16 +0200

    Gaphes TP1

--- 1-année/S2/TP/Graphes/TP1.py
+++ 1-année/S2/TP/Graphes/TP1.py
@@ -10,5 +10,15 @@ G.add_edge(1,2)
 G.add_edges_from([(1,3),(1,4),(2,3)])
 G.add_edge(1,5)
 
-nx.draw(G, with_labels=True)
+nx.draw(G, with_labels=True, node_color='yellow')
 plt.show()  
+
+print(G.nodes())
+
+print(G.number_of_nodes())
+
+print(G.degree(1))
+
+degrees = G.degree()
+
+print(sum(dict(degrees).values())/ G.number_of_nodes())
