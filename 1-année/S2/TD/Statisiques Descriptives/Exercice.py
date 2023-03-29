c54ffe42cfaa4383ad66f7e663ec0e050329adad
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-03-29 16:24:38 +0200

    Exo Stats

--- 1-année/S2/TD/Statisiques Descriptives/Exercice.py
+++ 1-année/S2/TD/Statisiques Descriptives/Exercice.py
@@ -13,5 +13,13 @@ Moyen_déplacement = ['Voiture','Transport en commun','Pieds', 'Velo', 'Autre']
 Pourcentage = [74,16,6,2,2]
 
 explode=(0.1,0.5,0.1,0.1,0.1)
-plt.pie(Pourcentage, explode=explode,shadow=True, labels=Moyen_déplacement)
+# plt.pie(Pourcentage, explode=explode,shadow=True, labels=Moyen_déplacement)
+# plt.show()
+
+#Exercice 3
+
+Fréquence =[1,6,8,10,11,12,13,15,16]
+Note =[0,2,1,3,2,4,2,2,1]
+
+plt.bar(Fréquence,Note,  width = 0.05, color='red')
 plt.show()
