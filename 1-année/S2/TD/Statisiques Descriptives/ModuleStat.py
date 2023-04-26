6b6f2d32d4df098c12e58cc6bd001c606e40cb08
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-04-26 17:50:10 +0200

    maj

--- 1-année/S2/TD/Statisiques Descriptives/ModuleStat.py
+++ 1-année/S2/TD/Statisiques Descriptives/ModuleStat.py
@@ -10,4 +10,12 @@ def moyenne(li, eff=[]):
     return somme/sommeCoef
 
 
-def mediane()
+def mediane(l):
+    l.sort()
+    if len(l)%2 == 0:
+        return (l[len(l)//2]+l[len(l)//2-1])/2
+    else:
+        return l[len(l)//2]
+    
+
+def quartiles
