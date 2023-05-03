36e641ba1373ceaf684b7fda465f342305ae0df0
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-05-03 16:44:39 +0200

    Maj

--- 1-année/S2/TD/Statisiques Descriptives/Exercice.py
+++ 1-année/S2/TD/Statisiques Descriptives/Exercice.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import csv
+from ModuleStat import moyenne
 
 #Exercice 1
 
@@ -22,4 +24,15 @@ Fréquence =[1,6,8,10,11,12,13,15,16]
 Note =[0,2,1,3,2,4,2,2,1]
 
 plt.bar(Fréquence,Note,  width = 0.05, color='red')
-plt.show()
+# plt.show()
+
+
+#Exercice 13
+
+with open('parc-informatique-2020.csv',"r",encoding="utf8") as csv_file:
+    contenu = csv_file.readlines()
+    contenu = [ligne.strip().split(";") for ligne in contenu]
+
+M = moyenne([int(elt[7]) for elt in contenu[1:] if elt[3] == "Photocopieur"])
+
+plt.pie(date_d_installation, explode=explode, shadow=true)
