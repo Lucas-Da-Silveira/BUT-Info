e67a3722feffd1335a1998dec7756ddbe6cc818d
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-05-10 13:45:36 +0200

    maj

--- 1-année/S2/TP/ProgObj/TP10/src/Joueur.java
+++ 1-année/S2/TP/ProgObj/TP10/src/Joueur.java
@@ -1,69 +1,43 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Joueur extends Cartes{
+public class Joueur {
+    private String nom;
+    private List<Carte> cartes;
 
-    protected String nom;
-    ArrayList<String> joueur1 = new ArrayList<String>(paquet.subList(0, 16));
-    ArrayList<String> joueur2 = new ArrayList<String>(paquet.subList(16, 32));
-
-    public Joueur(String nom){
+    public Joueur(String nom) {
         this.nom = nom;
+        this.cartes = new ArrayList<Carte>();
     }
 
-    public String getNom(){
-        return nom;
+    public void addCarte(Carte carte) {
+        cartes.add(carte);
     }
 
-    public String toString(){
-        String str = "";
-        for (int i = 0; i < paquet.size(); i++){
-            str += paquet.get(i) + "\n";
+    public void addCartes(List<Carte> cartes) {
+        this.cartes.addAll(cartes);
+        cartes.clear();
+    }
+
+    public boolean aDesCartes() {
+        return !cartes.isEmpty();
+    }
+
+    public Carte playCarte() {
+        if(this.cartes.isEmpty()) {
+            return null;
         }
-        return str;
+        return cartes.remove(0);
     }
 
-    public void ajouterCarte(String carte){
-        paquet.add(carte);
+    public String getNom() {
+        return this.nom;
     }
 
-    public String tirerCarte(){
-        String carte = paquet.get(0);
-        paquet.remove(0);
-        return carte;
+    public void shuffleDeck() {
+        Collections.shuffle(this.cartes);
     }
 
-    public int getNbCartes(){
-        return paquet.size();
-    }
-
-    public void melanger(){
-        for (int i = 0; i < nbCartes; i++){
-            int random = (int) (Math.random() * nbCartes);
-            String temp = paquet.get(i);
-            paquet.set(i, paquet.get(random));
-            paquet.set(random, temp);
-        }
-    }
-
-    public void jouerCarte(){
-        String carte = paquet.get(0);
-        paquet.remove(0);
-        System.out.println(nom + " joue " + carte);
-    }
-
-    public void gagnerCarte(String carte){
-        paquet.add(carte);
-    }
-
-    public boolean aPerdu(){
-        return paquet.isEmpty();
-    }
-
-    public void afficherCartes(){
-        System.out.println(nom + " a " + paquet.size() + " cartes");
-    }
-
-    public void afficherCarte(){
-        System.out.println(nom + " a " + paquet.get(0));
+    public List<Carte> getCartes() {
+        return this.cartes;
     }
 }
