5332e95e3152c92fd0ebe5ef793156ce40614dc4
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-05-10 14:36:59 +0200

    maj

--- 1-année/S2/TP/ProgObj/TP11(2)/src/Bibliotheque.java
+++ 1-année/S2/TP/ProgObj/TP11(2)/src/Bibliotheque.java
@@ -1,59 +1,67 @@
 public class Bibliotheque {
-    
-    private String [] bibliotheque;
+    private String nomBibliotheque;
+    private Document [ ] listeDocs; 
+    private int prochainNumero; 
+    private static final int CAPACITE = 100; 
 
-    public Bibliotheque(){
-        this.bibliotheque = new String[5];
-    }
 
-    public Bibliotheque(String[] bibliotheque) {
-        this.bibliotheque = bibliotheque;
-    }
-
-    public String[] getBibliotheque() {
-        return bibliotheque;
-    }
-
-    public void setBibliotheque(String[] bibliotheque) {
-        this.bibliotheque = bibliotheque;
-    }
-
-    public void ajouterDocument(Document document){
-        for (int i = 0; i < bibliotheque.length; i++){
-            if (bibliotheque[i] == null){
-                bibliotheque[i] = document.toString();
-                break;
+    public Document getDocument(int num) throws Exception{
+        for (Document doc : listeDocs){
+            if (doc != null && doc.getNumEnregistrement() == num){
+                return doc;
             }
         }
+        throw new Exception("Le document numéro " +num +" n'est pas dans la bibliothèque");
     }
 
-    public void afficherDocuments(){
-        for (int i = 0; i < bibliotheque.length; i++){
-            if (bibliotheque[i] != null)
-                System.out.println(bibliotheque[i]);
-        }
-    }
-
-    public void supprimerDocument(){
-        for (int i = 0; i < bibliotheque.length; i++){
-            if (bibliotheque[i] != null){
-                bibliotheque[i] = null;
-                break;
+    public Document retirerDocument(int num) throws Exception{
+        for(int i = 0; i < listeDocs.length; i++){
+            Document doc = listeDocs[i];
+            if (doc != null && doc.getNumEnregistrement() == num){
+                listeDocs[i] = null;
+                return doc;
             }
         }
+        throw new Exception("Le document numéro " +num+ " n'est pas dans la bibliothèque");
     }
 
-    public boolean afficheiemeDocument(){
-        for (int i = 0; i < bibliotheque.length; i++){
-            if (bibliotheque[i] != null){
-                System.out.println(bibliotheque[i]);
-                break;
-            }
-            else{
-                return false;
-            }
-        }   
-        return true;
+    public void ajouterDocument(Document d) throws Exception{
+        if (prochainNumero >= CAPACITE){
+            throw new Exception("La bibliothèque est pleine. Impossible d'ajouter un nouveau documents");
+        }
+        listeDocs[prochainNumero] = d;
+        prochainNumero++;
     }
 
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Bibliotheque bibli = new Bibliotheque();
+
+        // try {
+        //     Document document = bibli.getDocument(123); 
+        // } 
+        // catch (Exception e) {
+        //     System.out.println(e.getMessage());
+        // }
+
+        // try {
+        //     Document document = bibli.retirerDocument(123);
+        // } 
+        // catch (Exception e) {
+        //     System.out.println(e.getMessage());
+        // }
+
+        try {
+            Document document = new Document();
+            bibli.ajouterDocument(document);
+        } 
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
