4b32030ded6e6e56034dafaf16abceb426d3f012
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-05-15 18:58:14 +0200

    TP IHM

--- 1-année/S2/TP/IHM/TP4/src/ControlList.java
+++ 1-année/S2/TP/IHM/TP4/src/ControlList.java
@@ -1,2 +1,22 @@
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 public class ControlList {
+    private Model model;
+    private View view;
+    public ControlList(Model model, View view){
+        this.model = model;
+        this.view = view;
+
+        view.getListView().getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue )-> {
+            int selectIndex = view.getListView().getSelectionModel().getSelectedIndex();
+            if (selectIndex >= 0) {
+                model.setCurrentIndex(selectIndex);
+
+                String imageName = model.getImageNames().get(model.getCurrentIndex());
+                Image image = new Image(imageName);
+                view.getImageView().setImage(image);
+            }
+        });
+    }
 }
--- 1-année/S2/TP/IHM/TP4/src/Main.java
+++ 1-année/S2/TP/IHM/TP4/src/Main.java
@@ -6,6 +6,7 @@ public class Main  extends Application {
     public void start(Stage stage) {
         Model model = new Model();
         View view = new View(model);
+        ControlList controlList = new ControlList(model, view);
         stage.setScene(view.initialiseView());
         stage.show();
 
