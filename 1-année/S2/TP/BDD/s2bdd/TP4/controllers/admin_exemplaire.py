9a402e4e3fdc7d5f4cb3e29919e90974ec15998f
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-05-22 14:14:07 +0200

    maj

--- 1-année/S2/TP/BDD/s2bdd/TP4/controllers/admin_exemplaire.py
+++ 1-année/S2/TP/BDD/s2bdd/TP4/controllers/admin_exemplaire.py
@@ -7,48 +7,32 @@ from flask import *
 import re
 import datetime
 
+from validator.validor_exemplaire_etu import *
+from models.dao_exemplaire import *
+
 from connexion_db import get_db
 
 admin_exemplaire = Blueprint('admin_exemplaire', __name__,
-                        template_folder='templates')
+                             template_folder='templates')
+
 
 @admin_exemplaire.route('/admin/exemplaire/show')
 def show_exemplaire():
     id_oeuvre = request.args.get('id_oeuvre', '')
-    mycursor = get_db().cursor()
-    sql = ''' SELECT DISTINCT a.nom , o.titre, o.date_parution, COUNT(DISTINCT e.id_exemplaire) AS nb_exemp_dispo, 
-            (SELECT DISTINCT COUNT(*) FROM exemplaire WHERE oeuvre_id = o.id_oeuvre) AS nb_exemplaire, o.id_oeuvre AS nb_exemplaire
-        FROM oeuvre o
-        JOIN auteur a ON o.auteur_id = a.id_auteur
-        LEFT JOIN exemplaire e ON o.id_oeuvre = e.oeuvre_id AND e.etat <> 'EMPRUNT'
-        WHERE o.id_oeuvre = %s
-        GROUP BY o.id_oeuvre;
-'''
-    mycursor.execute(sql,(id_oeuvre))
-    oeuvre = mycursor.fetchone()
-    sql = ''' SELECT e.id_exemplaire, e.etat, e.date_achat, e.prix,
-                CASE WHEN e.etat <> 'EMPRUNT' THEN 'present' ELSE 'abs' END AS present
-            FROM exemplaire e
-            JOIN oeuvre o ON e.oeuvre_id = o.id_oeuvre
-            WHERE o.id_oeuvre = %s
- '''
-    mycursor.execute(sql,(id_oeuvre))
-    exemplaires = mycursor.fetchall()
+    oeuvre = find_details_oeuvre_exemplaires(id_oeuvre)
+    exemplaires = find_exemplaires_oeuvre(id_oeuvre)
     return render_template('admin/exemplaire/show_exemplaires.html', exemplaires=exemplaires, oeuvre=oeuvre)
+
 
 @admin_exemplaire.route('/admin/exemplaire/add', methods=['GET'])
 def add_exemplaire():
     id_oeuvre = request.args.get('id_oeuvre', '')
-    mycursor = get_db().cursor()
-    sql = ''' SELECT a.nom, o.titre, o.date_parution, o.id_oeuvre
-            FROM oeuvre o
-            JOIN auteur a ON o.auteur_id = a.id_auteur
-            WHERE o.id_oeuvre = %s  
- '''
-    mycursor.execute(sql, (id_oeuvre))
-    oeuvre = mycursor.fetchone()
+    oeuvre = find_add_exemplaire_info_oeuvre(id_oeuvre)
     date_achat = datetime.datetime.now().strftime("%d/%m/%Y")
-    return render_template('admin/exemplaire/add_exemplaire.html', oeuvre=oeuvre, exemplaire={'date_achat': date_achat, 'id_oeuvre': id_oeuvre}, erreurs=[])
+    return render_template('admin/exemplaire/add_exemplaire.html', oeuvre=oeuvre,
+                           exemplaire={'date_achat': date_achat, 'id_oeuvre': id_oeuvre}, erreurs=[])
+
+
 @admin_exemplaire.route('/admin/exemplaire/add', methods=['POST'])
 def valid_add_exemplaire():
     mycursor = get_db().cursor()
@@ -58,76 +42,48 @@ def valid_add_exemplaire():
     etat = request.form.get('etat', '')
     prix = request.form.get('prix', '')
 
-    dto_data={'id_oeuvre': id_oeuvre, 'etat': etat, 'date_achat': date_achat, 'prix': prix}
+    dto_data = {'id_oeuvre': id_oeuvre, 'etat': etat, 'date_achat': date_achat, 'prix': prix}
     valid, errors, dto_data = validator_exemplaire(dto_data)
     if valid:
         date_achat = dto_data['date_achat_iso']
-        tuple_insert = (id_oeuvre,etat,date_achat,prix)
-        print(tuple_insert)
-        sql = '''INSERT INTO exemplaire (oeuvre_id, etat, date_achat, prix) VALUES (%s,%s,%s,%s)'''
-        mycursor.execute(sql, tuple_insert)
-        get_db().commit()
-        message = u'exemplaire ajouté , oeuvre_id :'+str(id_oeuvre)
+        exemplaire_insert(etat, date_achat, prix, id_oeuvre)
+        message = u'exemplaire ajouté , oeuvre_id :' + str(id_oeuvre)
         flash(message, 'success radius')
-        return redirect('/admin/exemplaire/show?id_oeuvre='+str(id_oeuvre))
+        return redirect('/admin/exemplaire/show?id_oeuvre=' + str(id_oeuvre))
 
-    sql = ''' SELECT a.nom, o.titre, o.date_parution, o.id_oeuvre
-            FROM oeuvre o
-            JOIN auteur a ON o.auteur_id = a.id_auteur
-            WHERE o.id_oeuvre = %s
- '''
-    mycursor.execute(sql, (id_oeuvre))
-    oeuvre = mycursor.fetchone()
+    oeuvre = find_add_exemplaire_info_oeuvre(id_oeuvre)
     return render_template('admin/exemplaire/add_exemplaire.html', oeuvre=oeuvre,
                            exemplaire=dto_data, erreurs=errors)
 
+
 @admin_exemplaire.route('/admin/exemplaire/delete', methods=['GET'])
 def delete_exemplaire():
-    mycursor = get_db().cursor()
     id_exemplaire = request.args.get('id_exemplaire', '')
-    tuple_delete = (id_exemplaire,)
-    sql = '''SELECT oeuvre_id FROM exemplaire WHERE id_exemplaire =%s'''
-    mycursor.execute(sql, tuple_delete)
-    oeuvre = mycursor.fetchone()
-    oeuvre_id=str(oeuvre['oeuvre_id'])
-    print(oeuvre,oeuvre_id)
-    if not(oeuvre_id and oeuvre_id.isnumeric()):
-        abort("404","erreur id_oeuvre")
-    nb_emprunts = 0
-    sql = ''' SELECT COUNT(*) AS nb_emprunts FROM emprunt WHERE exemplaire_id = %s '''
-    mycursor.execute(sql, tuple_delete)
-    res_nb_emprunts = mycursor.fetchone()
-    if 'nb_emprunts' in res_nb_emprunts.keys():
-        nb_emprunts=res_nb_emprunts['nb_emprunts']
-    if nb_emprunts == 0 :
-        sql = ''' DELETE FROM exemplaire WHERE id_exemplaire = %s '''
-        mycursor.execute(sql, tuple_delete)
-        get_db().commit()
-        message=u'exemplaire supprimé, id: ' + id_exemplaire
+    oeuvre_id = find_id_oeuvre_exemplaire(id_exemplaire)
+    if not (oeuvre_id and oeuvre_id.isnumeric()):
+        abort("404", "erreur id_oeuvre")
+
+    nb_emprunts = find_exemplaire_nbEmprunts(id_exemplaire)
+
+    if nb_emprunts == 0:
+        exemplaire_delete(id_exemplaire)
+        message = u'exemplaire supprimé, id: ' + id_exemplaire
         flash(message, 'success radius')
-    else :
-        message=u'suppression impossible, il faut supprimer  : ' + str(nb_emprunts) + u' emprunt(s) de cet exemplaire'
+    else:
+        message = u'suppression impossible, il faut supprimer  : ' + str(nb_emprunts) + u' emprunt(s) de cet exemplaire'
         flash(message, 'warning')
-    return redirect('/admin/exemplaire/show?id_oeuvre='+oeuvre_id)
+    return redirect('/admin/exemplaire/show?id_oeuvre=' + oeuvre_id)
+
 
 @admin_exemplaire.route('/admin/exemplaire/edit', methods=['GET'])
 def edit_exemplaire():
-    mycursor = get_db().cursor()
     id_exemplaire = request.args.get('id_exemplaire', '')
-    sql = ''' SELECT a.nom AS nom_auteur, o.titre, o.date_parution, o.id_oeuvre AS noOeuvre
-             FROM oeuvre o
-             JOIN auteur a ON o.auteur_id = a.id_auteur
-             WHERE o.id_oeuvre = (SELECT id_oeuvre FROM exemplaire WHERE id_exemplaire = %s LIMIT 1)'''
-    mycursor.execute(sql, (id_exemplaire))
-    oeuvre = mycursor.fetchone()
-    sql = ''' SELECT *
-             FROM exemplaire
-             WHERE id_exemplaire = %s '''
-    mycursor.execute(sql, (id_exemplaire,))
-    exemplaire = mycursor.fetchone()
+    oeuvre = find_edit_details_oeuvre_id_exemplaire(id_exemplaire)
+    exemplaire = find_edit_one_exemplaire(id_exemplaire)
     if exemplaire['date_achat']:
-        exemplaire['date_achat']=exemplaire['date_achat'].strftime("%d/%m/%Y")
+        exemplaire['date_achat'] = exemplaire['date_achat'].strftime("%d/%m/%Y")
     return render_template('admin/exemplaire/edit_exemplaire.html', exemplaire=exemplaire, oeuvre=oeuvre, erreurs=[])
+
 
 @admin_exemplaire.route('/admin/exemplaire/edit', methods=['POST'])
 def valid_edit_exemplaire():
@@ -138,50 +94,16 @@ def valid_edit_exemplaire():
     etat = request.form.get('etat', '')
     prix = request.form.get('prix', '')
 
-    dto_data={'oeuvre_id': oeuvre_id, 'etat': etat, 'date_achat': date_achat, 'prix': prix, 'id_exemplaire':id_exemplaire}
+    dto_data = {'oeuvre_id': oeuvre_id, 'etat': etat, 'date_achat': date_achat, 'prix': prix,
+                'id_exemplaire': id_exemplaire}
     valid, errors, dto_data = validator_exemplaire(dto_data)
     print(valid, errors, dto_data)
     if valid:
         date_achat = dto_data['date_achat_iso']
-        tuple_update = (oeuvre_id, etat, date_achat, prix, id_exemplaire)
-        print(tuple_update)
-        sql = ''' UPDATE exemplaire
-                 SET oeuvre_id = %s, etat = %s, date_achat = %s, prix = %s
-                 WHERE id_exemplaire = %s '''
-        mycursor.execute(sql, tuple_update)
-        get_db().commit()
-        message=u' exemplaire modifié, id_exemplaire: ' + id_exemplaire
+        exemplaire_update(oeuvre_id, etat, date_achat, prix, id_exemplaire)
+        message = u' exemplaire modifié, id_exemplaire: ' + id_exemplaire
         flash(message, 'success radius')
-        return redirect('/admin/exemplaire/show?id_oeuvre='+oeuvre_id)
-    sql = ''' SELECT a.nom AS nom_auteur, o.titre, o.date_parution, o.id_oeuvre AS noOeuvre
-             FROM oeuvre o
-             JOIN auteur a ON o.auteur_id = a.id_auteur
-             WHERE o.id_oeuvre = (SELECT id_oeuvre FROM exemplaire WHERE id_exemplaire = %s LIMIT 1) '''
-    mycursor.execute(sql, (oeuvre_id))
-    oeuvre = mycursor.fetchone()
+        return redirect('/admin/exemplaire/show?id_oeuvre=' + oeuvre_id)
+
+    oeuvre = find_edit_details_oeuvre_id_exemplaire(oeuvre_id)
     return render_template('admin/exemplaire/edit_exemplaire.html', exemplaire=dto_data, oeuvre=oeuvre, erreurs=errors)
-
-
-def validator_exemplaire(data):
-    valid = True
-    errors = dict()
-    if 'id_exemplaire' in data.keys():
-        if not data['id_exemplaire'].isnumeric():
-            errors['id_exemplaire'] = 'type id incorrect(numeric)'
-            valid = False
-    if not re.match(r'\w{2,}', data['etat']):
-        errors['etat'] = "Le titre doit avoir au moins deux caractères"
-        valid = False
-    try:
-        datetime.datetime.strptime(data['date_achat'], '%d/%m/%Y')
-    except ValueError:
-        errors['date_achat'] = "la Date n'est pas valide format:%d/%m/%Y"
-        valid = False
-    else:
-        data['date_achat_iso'] = datetime.datetime.strptime(data['date_achat'], "%d/%m/%Y").strftime("%Y-%m-%d")
-    try:
-        float(data['prix'])
-    except ValueError:
-        errors['prix'] = "le Prix n'est pas valide"
-        valid = False
-    return (valid, errors, data)
--- 1-année/S2/TP/BDD/s2bdd/TP4/models/dao_exemplaire.py
+++ 1-année/S2/TP/BDD/s2bdd/TP4/models/dao_exemplaire.py
@@ -1,111 +1,160 @@
-
 from connexion_db import get_db
 from flask import *
+
 
 def find_details_oeuvre_exemplaires(id_oeuvre):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_1' FROM DUAL '''
-        cursor.execute(sql,(id_oeuvre))
+        cursor = connection.cursor()
+        sql = '''
+            SELECT
+                a.nom,
+                o.titre,
+                o.id_oeuvre,
+                o.date_parution,
+                COUNT(e.id_exemplaire) AS nb_exemplaire,
+                COUNT(e2.id_exemplaire) AS nb_exemp_dispo
+            FROM exemplaire e
+            LEFT JOIN oeuvre o ON e.oeuvre_id = o.id_oeuvre
+            LEFT JOIN exemplaire e2 ON e.id_exemplaire = e2.id_exemplaire
+            AND e2.id_exemplaire NOT IN (SELECT emprunt.exemplaire_id FROM emprunt WHERE emprunt.date_retour IS NULL)
+            LEFT JOIN auteur a ON a.id_auteur = o.auteur_id
+            WHERE o.id_oeuvre = %s;
+        '''
+        cursor.execute(sql, (id_oeuvre))
         return cursor.fetchone()
     except ValueError:
-        abort(400,'erreur requete 4_1')
+        abort(400, 'erreur requete 4_1')
+
 
 def find_exemplaires_oeuvre(id_oeuvre):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_2' FROM DUAL '''
-        cursor.execute(sql,(id_oeuvre))
+        cursor = connection.cursor()
+        sql = '''
+            SELECT
+                e.*,
+                o.titre,
+                o.id_oeuvre,
+                o.date_parution,
+                IF(COUNT(e2.id_exemplaire)=0, 'abs', 'present') AS present
+            FROM exemplaire e
+            LEFT JOIN oeuvre o ON e.oeuvre_id = o.id_oeuvre
+            LEFT JOIN exemplaire e2 ON e.id_exemplaire = e2.id_exemplaire
+            AND e2.id_exemplaire NOT IN (SELECT emprunt.exemplaire_id FROM emprunt WHERE emprunt.date_retour IS NULL)
+            WHERE o.id_oeuvre = %s
+            GROUP BY e.id_exemplaire
+            ORDER BY e.id_exemplaire DESC;
+        '''
+        cursor.execute(sql, (id_oeuvre))
         return cursor.fetchall()
     except ValueError:
-        abort(400,'erreur requete 4_2')
+        abort(400, 'erreur requete 4_2')
+
 
 def find_exemplaire_nbEmprunts(id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_7' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = 'SELECT COUNT(*) AS nb_emprunts FROM emprunt WHERE exemplaire_id = %s;'
         cursor.execute(sql, (id))
         res_nb_emprunts = cursor.fetchone()
         if 'nb_emprunts' in res_nb_emprunts.keys():
-            nb_emprunts=res_nb_emprunts['nb_emprunts']
+            nb_emprunts = res_nb_emprunts['nb_emprunts']
             return nb_emprunts
+        else:
+            abort(500, 'erreur requete 4_7')
     except ValueError:
-        abort(400,'erreur requete 4_7')
+        abort(400, 'erreur requete 4_7')
+
 
 def find_edit_one_exemplaire(id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_11' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = 'SELECT * FROM exemplaire WHERE id_exemplaire = %s;'
         cursor.execute(sql, (id))
         return cursor.fetchone()
     except ValueError:
-        abort(400,'erreur requete 4_11')
+        abort(400, 'erreur requete 4_11')
+
 
 def find_id_oeuvre_exemplaire(id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_9' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = 'SELECT oeuvre_id FROM exemplaire WHERE id_exemplaire = %s;'
         cursor.execute(sql, (id))
         oeuvre = cursor.fetchone()
-        oeuvre_id=str(oeuvre['oeuvre_id'])
+        oeuvre_id = str(oeuvre['oeuvre_id'])
         return oeuvre_id
     except ValueError:
-        abort(400,'erreur requete 4_9')
+        abort(400, 'erreur requete 4_9')
+
 
 def find_add_exemplaire_info_oeuvre(id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_3' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = '''
+            SELECT a.nom, o.titre, o.id_oeuvre, o.date_parution
+            FROM oeuvre o
+            LEFT JOIN auteur a ON o.auteur_id = a.id_auteur
+            WHERE o.id_oeuvre = %s;
+        '''
         cursor.execute(sql, (id))
         oeuvre = cursor.fetchone()
         return oeuvre
     except ValueError:
-        abort(400,'erreur requete 4_3')
+        abort(400, 'erreur requete 4_3')
+
 
 def find_edit_details_oeuvre_id_exemplaire(id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_10' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = '''
+            SELECT a.nom, o.titre, o.date_parution, o.id_oeuvre
+            FROM exemplaire e
+            LEFT JOIN oeuvre o ON e.oeuvre_id = o.id_oeuvre
+            LEFT JOIN auteur a ON o.auteur_id = a.id_auteur
+            WHERE e.id_exemplaire = %s;
+        '''
         cursor.execute(sql, (id))
         exemplaire = cursor.fetchone()
         return exemplaire
     except ValueError:
-        abort(400,'erreur requete 4_10')
+        abort(400, 'erreur requete 4_10')
 
 
-def exemplaire_insert(titre,dateParution,photo,idAuteur):
+def exemplaire_insert(etat, date_achat, prix, oeuvre_id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_5' FROM DUAL '''
-        cursor.execute(sql, (titre,dateParution,photo,idAuteur))
+        cursor = connection.cursor()
+        sql = 'INSERT INTO exemplaire(etat, date_achat, prix, oeuvre_id) VALUES(%s, %s, %s, %s);'
+        cursor.execute(sql, (etat, date_achat, prix, oeuvre_id))
         connection.commit()
     except ValueError:
-        abort(400,'erreur requete 4_5')
+        abort(400, 'erreur requete 4_5')
+
 
 def exemplaire_update(noOeuvre, etat, dateAchat, prix, id_exemplaire):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_12' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = 'UPDATE exemplaire SET oeuvre_id = %s, etat = %s, date_achat = %s, prix = %s WHERE id_exemplaire = %s;'
         cursor.execute(sql, (noOeuvre, etat, dateAchat, prix, id_exemplaire))
         connection.commit()
     except ValueError:
-        abort(400,'erreur requete 4_12')
+        abort(400, 'erreur requete 4_12')
+
 
 def exemplaire_delete(id):
     connection = get_db()
     try:
-        cursor=connection.cursor()
-        sql = ''' SELECT 'requete4_8' FROM DUAL '''
+        cursor = connection.cursor()
+        sql = 'DELETE FROM exemplaire WHERE id_exemplaire = %s;'
         cursor.execute(sql, (id))
         connection.commit()
     except ValueError:
-        abort(400,'erreur requete 4_8')
+        abort(400, 'erreur requete 4_8')
