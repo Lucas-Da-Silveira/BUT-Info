43a75cec2a3e8bf47f4b9321b6d41e6676bfce48
Lucas Da Silveira <lucas.da_silveira@edu.univ-fcomte.fr>
2023-09-05 10:22:34 +0200

    maj

---
+++ 2-année/S3/TD/Dev Web/Backend/TD1/app.js
@@ -0,0 +1,45 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
+
+
+console.log(process.argv);
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Utilisation de YARGS
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        //TODO
+    }
+});
+
+ 
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        //TODO
+    }
+})
+
+ 
+
+yargs.parse();
---
+++ 2-année/S3/TD/Dev Web/Backend/TD1/notes.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = false;
+
+    if (!duplicateNote) {
+
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+
+
+const saveNotes = (notes) => {
+    //TODO
+}
+
+const loadNotes = () => {
+    try {
+        //TODO
+    } catch (e) {
+        return []
+    }
+}
+
+const listNotes = () => {
+    //TODO
+}
+
+// EXPORT MODULES
