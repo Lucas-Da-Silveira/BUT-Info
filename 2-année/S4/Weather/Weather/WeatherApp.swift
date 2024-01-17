6082e2c82b3741f6d90fe07302e7c372f804ce14
Lucas Da Silveira <113794317+Lucas-Da-Silveira@users.noreply.github.com>
2024-01-17 16:44:05 +0100

    add iOS dev

---
+++ 2-année/S4/Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by da silveira lucas on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
