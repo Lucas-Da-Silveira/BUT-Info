cfc0554d1c44a12d79796e41450b82aa3f64f1fe
Lucas Da Silveira <lucasdslvra@gmail.com>
2024-01-29 15:40:22 +0100

    maj

---
+++ 2-année/S4/Qualité dev/PurseIntegration/src/test/java/integrationIzly/PurseIntegrationTest.java
@@ -0,0 +1,11 @@
+package integrationIzly;
+import org.junit.Test;
+
+public class PurseIntegrationTest {
+
+    @Test
+    public void testPurse() {
+        System.out.println("PurseIntegrationTest");
+    }
+
+}
